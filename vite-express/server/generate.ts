6ebf3241ba85f50168de6e7acc44ff11871a74c5
drodsou <gitman@drodsou.com>
2022-04-28 02:28:26 +0200

    generate md2html

--- vite-express/server/generate.ts
+++ vite-express/server/generate.ts
@@ -4,19 +4,23 @@ import { render } from './render-page';
 
 const __dirname = import.meta.url.split('/').slice(3, -1).join('/');
 
-const INDEX_HTML_PATH = path.resolve(__dirname, 'dist/static/index.html');
+const pagesDir = path.resolve(__dirname, '../client/pages');
+const distDir = path.resolve(__dirname, '../_dist');
 
+const readdirSyncRecursive = (d)=>  fs.readdirSync(d, {withFileTypes:true})
+  .map(f=>f.isDirectory() ? readdirSyncRecursive(d + '/' + f.name) : d + '/' + f.name)
+  .flat();
 
-(async () => {
-  // 1. Read
-  const template = fs.readFileSync(INDEX_HTML_PATH, 'utf-8');
+let re = /pages(\/(.*)\.(md|html))$/
+let pages = readdirSyncRecursive(pagesDir)
+  .filter(e=> e.match(re))
+  .map(e=>e.match(re)[1].replace('.md','.html'));
 
-  // 2. Render
-  const appHtml = await render('/');
-  const html = template.replace('<!--server-outlet-->', appHtml);
+;(async () => {
 
-  // 3. Write
-  fs.writeFileSync(INDEX_HTML_PATH, html);
-  console.log('Generated index.html');
-  console.log('`npx serve dist/static` to run static server');
+  for (let url of pages) {
+    let html = await render(url);
+    fs.writeFileSync(distDir + url, html);
+    console.log(`Generated ${distDir + url}`);
+  }
 })();
--- vite-express/server/render-page.ts
+++ vite-express/server/render-page.ts
@@ -3,7 +3,9 @@ import {withLayout} from './render-layouts';
 import fs from 'fs';
 const __dirname = import.meta.url.split('/').slice(3,-1).join('/');
 
-export async function render(url, context) {
+export async function render(url, context={}) {
+
+  
 
   // -- parse requested url into specific file
   const urlObj = new URL(url, 'http://localhost');
@@ -17,6 +19,8 @@ export async function render(url, context) {
 
   // -- check if related file exists (.html o .md)
   let pagePath = __dirname + '/../client/pages' + urlPath
+
+
   if (fs.existsSync(pagePath + '.html')) {
     pagePath += '.html';
   } else if (fs.existsSync(pagePath + '.md')) {
@@ -26,7 +30,11 @@ export async function render(url, context) {
   }
 
   // -- file exists, read file and parse content
+
+    
   let pageContent = fs.readFileSync(pagePath, 'utf-8');
+
+
 
   // -- parse frontmatter
   const fmRegex = /^---\n([\s\S]*?)\n---\n/
@@ -38,15 +46,19 @@ export async function render(url, context) {
   
   // -- add additional Fm entries from server loader, if exist
   let pageLoaderPath = pagePath.replace(/(\.md|\.html)$/, '.ts');
+  
+
   if (fs.existsSync(pageLoaderPath)) {
-    let loaderFmObj = await import(pageLoaderPath).then(m=>m.frontmatter());
+
+    console.log({pageLoaderPath})
+    let loaderFmObj = await import('file://' + pageLoaderPath).then(m=>m.frontmatter());
     pageFmObj = {...pageFmObj, ...loaderFmObj}
 
     // auto add loader script tag for client
     let clientScriptPath = pageLoaderPath.match(/\/client\/.*$/)[0]
     pageContent += `<script type="module" src="${clientScriptPath}"></script>`
   }
-
+  console.log('--what3')    
   // -- check frontmatter defaults
   if (!pageFmObj.lang) { pageFmObj.lang = 'en'; }
   if (!pageFmObj.title) { pageFmObj.title = 'missing title'; }
@@ -56,7 +68,7 @@ export async function render(url, context) {
   if (pagePath.endsWith('.md')) {
     pageBody = marked.parse(pageBody)
   }
-
+  
   // -- add layout
   let pageHtml = withLayout(pageBody, pageFmObj.layout ?? 'main');
 
@@ -65,6 +77,8 @@ export async function render(url, context) {
     pageHtml = pageHtml.replaceAll(`{{${fmEntry[0]}}}`, fmEntry[1]);
   });
   
+  
+
   return pageHtml;
   
 }
